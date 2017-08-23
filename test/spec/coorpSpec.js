78c271f177bcf7c8120fb2dd40f497a5fcb2eadf
Olivier Moreau <olivier.moreau@coorpacademy.com>
2017-08-23 11:42:45 +0200

    Modifying test
    There was an assert error coming with a bad name specified for the page.
    Resolved by changing assertion.
    (Merci Loïc )

--- test/spec/coorpSpec.js
+++ test/spec/coorpSpec.js
@@ -7,7 +7,7 @@ describe('[Access]', function() {
         return browser.url('https://www.coorpacademy.com')
         .getTitle()
         .then(function(title) {
-            assert.equal(title, 'Coorpacademy');
+            assert.ok(title.indexOf('Coorpacademy') >= 0, 'can\'t find Coorpacademy in title:' + title);
         });
     });
 });
